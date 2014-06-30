16f537632385735d51c60941c7d109266bbe0c60
karl tsou <karl.funlab@gmail.com>
2014-06-30 16:37:37 +0800

    cvs : to convert string refs to a list       calculate max, min and average refs

--- csv1.py
+++ csv1.py
@@ -6,18 +6,29 @@ reader = csv.reader(ifile)
 
 column = 0
 rowhead = 0
+refs = []
 
-# Parse square matrix refs
+# parse square matrix refs
 for row in reader:
 	if rowhead == 0:
-		# Save 1st row header
+		# save 1st row header
 		rowhead = row;
 	else:
-		# Work with 2th row(column)
+		# work with 2th row(column)
 		for col in row:
-			print "%s - %s" %(rowhead[column],col)
+			# skip first two columns
+			if column >= 2:
+				# skip content is zero
+				if col != "0":
+					# skip last empty
+					if col != "":
+						#print "%s - %s" %(rowhead[column],col)
+						refs.append(int(col))
 			column += 1
 
+# print the refs that to be used
+print "Refs - Max %d" % max(refs)
+print "Refs - Avg %f" % float(sum(refs) / len(refs))
+print "REfs - Min %d" % min(refs)
+
 ifile.close()
-
-
